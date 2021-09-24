ea582828e04f1f297b8205b729a0222ee7fb9cf9
kashi <24215309+kashi03@users.noreply.github.com>
2021-09-24 16:50:00 +0900

    update

---
+++ pages/test/hello.tsx
@@ -0,0 +1,38 @@
+import type { NextPage } from 'next'
+import { FormEvent, useState } from 'react'
+
+function test2(): String {
+  return String(process.env.a);
+}
+
+const Home: NextPage = () => {
+  const [name, setName] = useState('')
+  const registerUser = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    const res = await fetch(
+      '/api/hello?name='+event.target.name.value,
+      {
+        method: 'GET'
+      }
+    )
+
+    const result = await res.json()
+    console.log(result.name)
+    setName(result.name)
+    // result.user => 'Ada Lovelace'
+  }
+
+  return (
+    <>
+      <p>{ name }</p>
+      <form onSubmit={registerUser}>
+        <label htmlFor="name">Name</label>
+        <input id="name" name="name" type="text" autoComplete="name" required/>
+        <button type="submit">Register</button>
+      </form>
+    </>
+  )
+}
+
+export default Home
